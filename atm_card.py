47e698ec2f4b4707219e9edeee6e6a225b346ed3
Puspita Priatiningtyaz <ppriatiningtyaz@gmail.com>
2021-12-15 10:59:26 +0700

    add comment

--- atm_card.py
+++ atm_card.py
@@ -2,9 +2,9 @@ class ATMCard:
     def __init__(self, defaultPin, defaultBalance):
         self.defaultPin = defaultPin
         self.defaultBalance = defaultBalance
-
+#cek pin
     def cekPinAwal(self):
         return self.defaultPin
-
+#cek saldo awal
     def cekSaldoAwal(self):
         return self.defaultBalance
